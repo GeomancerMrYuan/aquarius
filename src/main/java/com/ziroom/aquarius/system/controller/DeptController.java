a3e76423f43ec5d40d71f4305f232590ff3b9031
yuanpeng <651159667@qq.com>
2020-05-23 15:25:51 +0800

    SpringBoot--整合redisson添加分布式锁

--- src/main/java/com/ziroom/aquarius/system/controller/DeptController.java
+++ src/main/java/com/ziroom/aquarius/system/controller/DeptController.java
@@ -4,6 +4,8 @@ package com.ziroom.aquarius.system.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ziroom.aquarius.common.annotation.LogAnnotation;
+import com.ziroom.aquarius.common.exception.CustomerException;
+import com.ziroom.aquarius.common.util.RedissonUtil;
 import com.ziroom.aquarius.common.vo.BaseResult;
 import com.ziroom.aquarius.system.entity.Dept;
 import com.ziroom.aquarius.system.service.IDeptService;
@@ -14,6 +16,7 @@ import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +24,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -42,34 +46,51 @@ public class DeptController {
     @Autowired
     private ProduceFactory factory;
 
+    @Autowired
+    private RedissonUtil redissonUtil;
+
     /**
      * 根据id获取部门信息
+     *
      * @param deptId
      * @return
      */
     @GetMapping("/getDeptById/{deptId}")
     @ApiOperation("根据id获取部门信息")
-//    @LogAnnotation(intoDB = true,description ="根据id获取部门信息" )
-    public BaseResult getDeptById(@PathVariable @ApiParam("部门id") Long deptId) {
-        Dept dept = deptService.getByDeptId(deptId);
-//        factory.select("rocket").sendMsg("根据id获取部门信息");
-        return BaseResult.success(dept);
+    public BaseResult getDeptById(@PathVariable @ApiParam("部门id") Long deptId) throws InterruptedException {
+        //增加分布式锁
+        RLock lock = redissonUtil.getReentLock(deptId.toString());
+        boolean flag = lock.tryLock(2, TimeUnit.SECONDS);
+        if (!flag) {
+            log.info("根据id获取部门信息任务产生了并发,该线程未获取到redis分布式锁");
+            throw new CustomerException(BaseResult.CodeEnums.FAILED, "任务执行中,请稍后重试");
+        }
+        try {
+            Dept dept = deptService.getByDeptId(deptId);
+            return BaseResult.success(dept);
+        } finally {
+            lock.unlock();
+        }
     }
 
     /**
      * 新增部门信息
+     *
      * @param dept
      * @return
      */
     @PostMapping("/add")
     @ApiOperation("新增部门信息")
+    @LogAnnotation(intoDB = true, description = "新增部门信息")
     public BaseResult addDept(@RequestBody @Valid Dept dept) {
         deptService.save(dept);
+        factory.select("rocket").sendMsg("新增部门信息");
         return BaseResult.success();
     }
 
     /**
      * 修改部门信息
+     *
      * @param dept
      * @return
      */
@@ -93,10 +114,9 @@ public class DeptController {
     }
 
 
-
     @PostMapping("/getPage")
-    public BaseResult getDeptPage(Long current, Long size, Dept dept){
-        Page<Dept> page = new Page<>(current,size);
+    public BaseResult getDeptPage(Long current, Long size, Dept dept) {
+        Page<Dept> page = new Page<>(current, size);
         QueryWrapper<Dept> wrapper = new QueryWrapper<>(dept);
         Page<Dept> result = deptService.page(page, wrapper);
         return BaseResult.success(result);
